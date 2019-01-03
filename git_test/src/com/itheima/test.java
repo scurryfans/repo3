9bfb3470f16a77a8ed377272c65b0a4133c5b523
FengKeRong <406871761@qq.com>
2019-01-03 19:26:51 +0800

    添加项目

---
+++ git_test/src/com/itheima/test.java
@@ -0,0 +1,17 @@
+package com.itheima;
+
+/**
+ * 类描述:
+ * 创建人:    fkr
+ * 创建时间:  2019/1/3 19:23
+ */
+public class test {
+
+    public static void main(String[] args) {
+        int i = 100;
+        while (i >= 5){
+            System.out.println(i);
+            i -= 5;
+        }
+    }
+}
