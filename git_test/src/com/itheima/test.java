25e3c20af506ad455d4099bef0853b64b4c503b0
FengKeRong <406871761@qq.com>
2019-01-03 21:13:30 +0800

    添加分支

--- git_test/src/com/itheima/test.java
+++ git_test/src/com/itheima/test.java
@@ -8,8 +8,9 @@ package com.itheima;
 public class test {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
-            System.out.println(i);
-        }
+        System.out.println("wowkwioowwow");
+        System.out.println("woowoowow");
+        System.out.println("hehe");
+        System.out.println("woheni");
     }
 }
